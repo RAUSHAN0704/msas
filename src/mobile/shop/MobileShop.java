0655dd00b812a3ef05986b10964a8f420c48e466
RAUSHAN0704 <raushan7490@gmail.com>
2015-03-10 18:40:18 +0530

    Version 0.1
    Basic One

---
+++ src/mobile/shop/MobileShop.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mobile.shop;
+
+/**
+ *
+ * @author RAUSHAN
+ */
+public class MobileShop {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
